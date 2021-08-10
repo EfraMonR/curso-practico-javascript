3f2c1cb8b3e8addf9953c92af6185cf8fea1b26f
Efrain Montealegre <efrain.montealegre@gmail.com>
2021-08-09 22:19:41 -0500

    Se crea función para calcular descuento con cupones, se utilizar arreglos y switch case

--- descuentos.js
+++ descuentos.js
@@ -11,6 +11,7 @@ const precioConDescuento = (precioOriginal * porcentajePrecioConDescuento) / 100
     precioConDescuento,
 })*/
 
+//Funciones para hacer descuento con valores
 function calcularPrecioConDescuento(precio, descuento) {
     const porcentajePrecioConDescuento = 100 - descuento;
     const precioConDescuento = (precio * porcentajePrecioConDescuento) / 100;
@@ -29,3 +30,37 @@ function onClickButtonPriceDiscount() {
     const resultP = document.getElementById("ResultP");
     resultP.innerText = "El precio con descuento son: $" + precioConDescuento;
 }
+
+//Funciones para hacer descuento con Cupones
+const coupons = [
+    "blue",
+    "red",
+    "black",
+];
+
+function onClickButtonCouponDiscount() {
+    const inputPrice = document.getElementById("InputPrice2");
+    const priceValue = inputPrice.value;
+
+    const inputCoupon = document.getElementById("InputCoupon");
+    const couponValue = inputCoupon.value;
+
+    let descuento;
+
+    switch(couponValue) {
+        case "blue":
+            descuento = 15;
+        break;
+        case "red":
+            descuento = 30;
+        break;
+        case "black":
+            descuento = 50;
+        break;
+    }
+
+    const precioConDescuento = calcularPrecioConDescuento(priceValue, descuento);
+    const resultP2 = document.getElementById("ResultP2");
+    resultP2.innerText = "El precio con descuento es: $" + precioConDescuento; 
+}
+
